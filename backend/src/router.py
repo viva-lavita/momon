9e918327d88505255d8c28661db3abecc6cfa1b7
vi.vala.vi <sovestyaaaa@gmail.com>
2025-06-07 22:07:56 +0300

    add docker

--- backend/src/router.py
+++ backend/src/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from src.app.router import router as utils_router
 from src.auth.router import router as auth_router
 from src.users.router import router as users_router
 
@@ -11,3 +12,4 @@ router = APIRouter(
 
 router.include_router(users_router)
 router.include_router(auth_router)
+router.include_router(utils_router)
